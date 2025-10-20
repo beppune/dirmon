2db7333d88af45f1c800c672c58a2b53a3129908
manzogi9 <manzogi9@posteitaliane.it>
2025-10-20 16:31:03 +0200

    make pipe optional. Help message

--- src/bgmon.rs
+++ src/bgmon.rs
@@ -1,6 +1,6 @@
 mod config;
 
-use named_pipe::PipeServer;
+use named_pipe::{ConnectingServer, PipeServer};
 use notify::{Event, ReadDirectoryChangesWatcher, RecursiveMode, Result, Watcher};
 use std::{path::PathBuf, sync::mpsc::Receiver};
 use std::sync::mpsc;
@@ -21,16 +21,27 @@ fn main() -> Result<()> {
     // ARGS
     let mut args = pico_args::Arguments::from_env();
     if args.contains(["-h", "--help"]) {
-        println!("\nbgmon [-c <config file> (default: .\\dirmon.toml)]\n");
+        println!(r#"
+     bgmon [-c <config file> (default: .\\dirmon.toml)]
+
+     Other options:
+         -h, --help:     Show this help
+         -n, --nopipe:   No pipe will be used
+"#);
         std::process::exit(0);
     }
     let config_file:Option<String> = args.value_from_str("-c").ok();
 
     // CONFIGURATION
     // Read Config
-    let config = config::load( config_file ).unwrap();
+    let mut config = config::load( config_file ).unwrap();
     let pipe_name = format!("\\\\.\\pipe\\{}", config.pipe_name.clone());
-    //
+
+    // Override options
+    if args.contains(["-n", "--nopipe"]) {
+        config.nopipe = true;
+    }
+
     // LOGGING
     let _ = CombinedLogger::init(vec![
         SimpleLogger::new(LevelFilter::Info, Config::default()),
@@ -52,11 +63,20 @@ fn main() -> Result<()> {
         watchers.push( (rx, w) );
     }
 
-    // PIPES
-    let mut pipe = named_pipe::PipeOptions::new(&pipe_name)
-        .single().unwrap().wait().unwrap();
-    info!("Opened Pipe [{pipe_name}]");
+    // PIPE
+    let pipe_server:ConnectingServer;
+    let mut pipe:Option<PipeServer> = None;
 
+    if !config.nopipe {
+        pipe_server = named_pipe::PipeOptions::new(&pipe_name)
+            .single().unwrap() ;
+        pipe = Some( pipe_server.wait().unwrap() );
+        info!("Opened Pipe [{pipe_name}]");
+    } else {
+        info!("Pipe is disabled [--nopipe, see help]")
+    }
+
+    info!("Watching directories.");
     loop {
         for (rec, _) in &watchers {
             let tryrecv = rec.try_recv();
@@ -67,8 +87,8 @@ fn main() -> Result<()> {
             match tryrecv.unwrap() {
                 Ok(event) => {
                     match event.kind {
-                        notify::EventKind::Create(_) => run_create(event, &mut pipe),
-                        notify::EventKind::Remove(_) => run_remove(&config.dirconfs, event, &mut pipe),
+                        notify::EventKind::Create(_) => run_create(event, pipe.as_mut()),
+                        notify::EventKind::Remove(_) => run_remove(&config.dirconfs, event, pipe.as_mut()),
                         // notify::EventKind::Any => todo!(),
                         // notify::EventKind::Access(access_kind) => todo!(),
                         // notify::EventKind::Modify(modify_kind) => todo!(),
@@ -86,7 +106,7 @@ fn main() -> Result<()> {
 
 }
 
-fn run_create(ev:Event, pipe: &mut PipeServer) {
+fn run_create(ev:Event, pipe: Option<&mut PipeServer>) {
     let info = fs::metadata(ev.paths[0].as_path()).unwrap();
     let ftype = if info.file_type().is_dir() {
         "DIR"
@@ -95,11 +115,14 @@ fn run_create(ev:Event, pipe: &mut PipeServer) {
     };
     let ss = format!("Created {}: {}", ftype, ev.paths[0].to_str().unwrap());
     info!("{ss}");
-    pipe.write_all(ss.as_bytes()).unwrap();
-    pipe.write_all(b"\n").unwrap();
+
+    if let Some(p) = pipe {
+        p.write_all(ss.as_bytes()).unwrap();
+        p.write_all(b"\n").unwrap();
+    }
 }
 
-fn run_remove(dirconfs:&HashMap<PathBuf,HashMap<FsEvent,String>>, ev:Event, pipe: &mut PipeServer) {
+fn run_remove(dirconfs:&HashMap<PathBuf,HashMap<FsEvent,String>>, ev:Event, pipe: Option<&mut PipeServer>) {
     let path = &ev.paths[0];
     let parent = path.parent().unwrap();
 
@@ -107,8 +130,11 @@ fn run_remove(dirconfs:&HashMap<PathBuf,HashMap<FsEvent,String>>, ev:Event, pipe
 
     let ss = format!("Removed: {}", path.to_str().unwrap());
     info!("{ss}");
-    pipe.write_all(ss.as_bytes()).unwrap();
-    pipe.write_all(b"\n").unwrap();
+    
+    if let Some(p) = pipe {
+        p.write_all(ss.as_bytes()).unwrap();
+        p.write_all(b"\n").unwrap();
+    }
 }
 
 
